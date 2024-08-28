3fb3778049d20909d67fad57e8caae698349faf4
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-08-29 00:06:41 +0300

    Updated the components, SearchBar still in the works

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,12 +1,49 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ searchInput }) {
+  
+  const [products, setProducts] = useState([]);
+  
+  useEffect(() => {
+    fetch("http://localhost:3000/listings")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+        console.log(data);
+      });
+  }, []);
+
+  function handleDelete(id) {
+    const filteredProducts = products.filter((product) => {
+      return product.id !== id;
+    });
+
+    setProducts(filteredProducts);
+  }
+
+  const filteredSearchProducts = products.filter((product) => {
+    
+    return product.description
+      .includes(searchInput);
+  });
+
+  const productsToDisplay = filteredSearchProducts.map((product) => {
+    
+    return (
+
+      <ListingCard
+        key={product.description}
+        product={product}
+        handleDelete={handleDelete}
+      />
+
+    );
+  });
   return (
+
     <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
-      </ul>
+      <ul className="cards">{productsToDisplay}</ul>
     </main>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Search() {
+function Search({ handleSearch  }) {
+  const [search, setSearch] = useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
     console.log("submitted");
+    handleSearch(search);
   }
 
   return (
-    <form className="searchbar" onSubmit={handleSubmit}>
+    <form className="searchbar" 
+    onSubmit={handleSubmit}
+    >
+      
       <input
         type="text"
         id="search"
         placeholder="search free stuff"
-        value={""}
-        onChange={(e) => console.log(e.target.value)}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
-      <button type="submit">🔍</button>
+
+      <button 
+      type="submit">
+        🔍
+      </button>
+
     </form>
   );
 }
